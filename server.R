972cdc83928c0f8f58741975404962923c1bdffb
UrsaMinor-R <equisetum@protonmail.com>
2018-05-25 16:00:56 +0200

    Drobne upravy/velkost pisma

--- server.R
+++ server.R
@@ -2,6 +2,11 @@
 
 shinyServer(function(input, output) {
   
+  
+  # zoznam farieb pre jendotlive skupiny tajchov
+  dirColors <-c("1"="#595490", "2"="#527525", "3"="#A93F35", "4"="#BA48AA")
+  
+  
   #This function is repsonsible for loading in the selected file
   filedata <- reactive({
     infile <- input$datafile
@@ -11,26 +16,38 @@ shinyServer(function(input, output) {
     }
     
     # shapeData <- readOGR(".",'polyline')
-    shapeData <- readShapeLines('polyline')
+    shpTajchy <- readOGR(".",'tajchy_tab')
     # ogrInfo(".",'polyline')
-    shapeData <- spTransform(shapeData, CRS("+proj=longlat +datum=WGS84 +no_defs"))
+    shpTajchy <- spTransform(shpTajchy, CRS("+proj=longlat +datum=WGS84 +no_defs"))
+    dtTajchy <- data.table::setDT(as.data.frame(shpTajchy))
+
+    
     
     
   })
   
   
-  
-  
-
   output$mymap <- renderLeaflet({
     
     input$update   # catching the action button event
     isolate(leaflet() %>%
               addProviderTiles(input$bmap)) %>%
-      setView(lng = 18.9, lat = 48.46, zoom = 12) %>% 
-    addPolygons(data=shapeData,weight=1,col = 'blue') %>% 
+      setView(lng = 18.9, lat = 48.458, zoom = 14) %>% 
+    addPolygons(data=shpTajchy,weight=1,col = 'blue') %>% 
     addMarkers(lng = 18.9, lat = 48.46,popup="Hi there")
   })
   
   
+  # Create data table
+  output$infoTable <- DT::renderDataTable({
+    req(input$skupina)
+    data_from_selected_tajch <- dtTajchy %>%
+      filter(skupina %in% input$skupina) %>%
+      select("name", "kupanie")
+    DT::datatable(data = data_from_selected_tajch, 
+                  options = list(pageLength = 10), 
+                  rownames = FALSE)
+  })
+  
+ 
 })
--- ui.R
+++ ui.R
@@ -5,18 +5,18 @@ shinyUI(bootstrapPage(
   
   includeCSS("styles.css"),
   
-  # navbarPage(title = " Tajchy (artificial water reservoirs) of Banská Štiavnica"),
-  header <- dashboardHeader(
-    title = "Twin Cities Buses"
-  ),
+  navbarPage(title = " Tajchy (artificial water reservoirs) of Banská Štiavnica"),
+  # header <- dashboardHeader(
+  #   title = "Twin Cities Buses"
+  # ),
   
   
-  leafletOutput("mymap", width="100%", height="90%"),
+  leafletOutput("mymap", width="80%", height="90%"),
   
   
   
-  absolutePanel(id = "control", top = 120, left = "auto", right = 50, bottom = "auto",
-                width = 250, height = "auto",
+  absolutePanel(id = "control", top = 100, left = "auto", right = 50, bottom = "auto",
+                width="16%", height = "auto",
                 fixed = TRUE, draggable = TRUE,
                 
                 selectInput("bmap", "Base map tile provider", choices = c("OpenStreetMap.Mapnik",
@@ -27,16 +27,19 @@ shinyUI(bootstrapPage(
                                                                           "Stamen.Terrain",
                                                                           "Stamen.Toner"), 
                             selected = "OpenStreetMap.Mapnik"),
-                actionButton("update", "Update Map!")),
-  
-  absolutePanel(id = "control2", top = 270, left = "auto", right = 50, bottom = "auto",
-                width = 250, height = "auto",
-                fixed = TRUE, draggable = TRUE,
+                actionButton("update", "Update Map!"),
                 
                 # SELECTINPUT - VYBER SKUPINY TAJCHOV a JARKOV ----------------------------
                 selectInput(inputId ="skupina",
-                            label = "Skupina tajchov", c("Piargske", "Štiavnické", "Hodrušské", "Vyhnianske", "Belianske", "Kolpaššské", "Moderštôlnianske", "Pukanské"),
-                            selectize = TRUE, selected = "Štiavnické", multiple = TRUE),
+                            label = "Skupina tajchov", c("Piargske" = "piargske",
+                                                         "Štiavnické"="stiavnicke",
+                                                         "Hodrušské"="hodrusske",
+                                                         "Vyhnianske"="vyhnianske",
+                                                         "Belianske"="belianske",
+                                                         "Kolpaššské"="kolpasske",
+                                                         "Moderštôlnianske"="modersolnianske",
+                                                         "Pukanské"="pukanske"),
+                            selectize = TRUE, selected = "stiavnicke", multiple = TRUE),
                 # SLIDER ------------------------------------------------------------------
                 sliderInput("elev", "Elevation (m n.m.)",
                             min = 400, max = 1000, value = c(400,800)),
@@ -45,16 +48,28 @@ shinyUI(bootstrapPage(
                 checkboxGroupInput("checkbox", "Objects to show:",
                                    c("Tajchy" = "tjch",
                                      "Jarky" = "jrk",
-                                     "Štôlne" = "stl")),
+                                     "Štôlne" = "stl"),
+                                   selected = "tjch"),
                 
                 tags$div(class="header", checked=NA,
                          tags$p("Ready to take the Shiny tutorial? If so"),
                          tags$a(href="shiny.rstudio.com/tutorial", "Click Here!")
                 )
+                
+                ),
+  
+  absolutePanel(id = "AbsTableInfo", top = 700, left = "auto", right = 50, bottom = "auto",
+                width="16%", height = "auto",
+                fixed = TRUE, draggable = TRUE,
+                
+                DT::dataTableOutput(outputId = "infoTable")
+
+                )
+                
+  )
+
+
 
   
   )
-  
-  
-  ))
-
+ 2
