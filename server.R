85b3e0d271af0aadbd8b9574e31e0fea0cded1f6
UrsaMinor-R <equisetum@protonmail.com>
2018-05-28 14:51:16 +0200

    :( set multiple from server

--- server.R
+++ server.R
@@ -6,28 +6,92 @@ server <- function(input, output, session) {
   # zoznam farieb pre jendotlive skupiny tajchov
   # dirColors <-c("1"="#595490", "2"="#527525", "3"="#A93F35", "4"="#BA48AA")
   
-  # Create reactive data frame
+  # range_selected <- reactive({
+  #   req(input$range)
+  # })
+  
+  
+  # Reactive element - ROZSAH ROKOV -----------------------------------------
+  group_selected <- reactive({
+    req(input$skupina)
+    if (input$skupina == "celySystem"){
+      shpTajchy
+    }
+    else {
+      shpTajchy[shpTajchy$skupina == input$skupina, ]
+    }
+    # shpTajchy$vznik > as.Date(as.POSIXct(paste(input$range[1], "-01-01", sep=""), origin = "1960-10-01")) &
+  })
+  
+  
+# Reactive element - ROZSAH ROKOV -----------------------------------------
   shp_selected <- reactive({
     req(input$range)
-    shpTajchy[shpTajchy$vznik > as.Date(as.POSIXct(paste(input$range[1], "-01-01", sep=""), origin = "1960-10-01")) &
-                shpTajchy$vznik < as.Date(as.POSIXct(paste(input$range[2], "-01-01", sep=""), origin = "1960-10-01")),]
+    textRange <- paste0(input$range, "-01-01", sep="")
+    group_selected()[group_selected()$vznik < as.Date(textRange),]
+    # shpTajchy$vznik > as.Date(as.POSIXct(paste(input$range[1], "-01-01", sep=""), origin = "1960-10-01")) &
   })
+
+    
+  # Reactive element - ROZSAH ROKOV -----------------------------------------
+  # budAlebo <- reactiveValues(
+  #   req(input$skupina),
+  #   if(input$skupina == "celySystem") {
+  #     FALSE
+  #   } else {
+  #     TRUE
+  #   }
+  # )
+  
+  # Can also set the label and select items
+  # updateSelectInput(session, "inSelect",
+  #                   label = paste("Select input label", length(x)),
+  #                   choices = x,
+  #                   selected = tail(x, 1)
+  # )
+  vyber <- reactive({
+    req(input$skupina)
+    input$skupina
+  })
+  
+  output$multiple <- reactive({
+      if(vyber() == "celySystem") {
+        return(FALSE)
+      } else {
+        return(TRUE)
+      }
+  })
+  
+  outputOptions(output, "multiple", suspendWhenHidden = FALSE)  
+
   
   # Create the plot
   output$map <- renderLeaflet({
     
-    isolate(leaflet() %>%
-              addProviderTiles(input$bmap) %>% 
-              setView(lng = 18.9, lat = 48.458, zoom = 14)) %>%
-    addPolygons(
-      data = shp_selected(),
-      weight = 2,
-      col = 'red',
-      label = shp_selected()$name %>%
-      addMarkers(lng = 18.9, lat = 48.46, popup = "Hi there")
-    )
-  })
+      input$update   # catching the action button event
+      isolate(leaflet() %>%
+                addProviderTiles(input$bmap))  %>% 
+        setView(lng = 18.9, lat = 48.458, zoom = 14) %>% 
+        addPolygons(
+          data = shp_selected(),
+          weight = 1,
+          col = "blue"
+          # label = shpTajchy$name
+        )
+      
+    })
+    
+
+
   
+  #   data = shp_selected(),
+    #   weight = 2,
+    #   col = 'red',
+    #   label = shp_selected() %>% #$name
+    #   addMarkers(lng = 18.9, lat = 48.46, popup = "Hi there")
+    # )
+
+
   
 }
 
--- ui.R
+++ ui.R
@@ -12,69 +12,85 @@ max_date <- max(as.numeric(as.character(shpTajchy$vznik)))
 shpTajchy$vznik <- zoo::as.Date(zoo::as.yearmon(shpTajchy$vznik, "%Y"), origin = "1960-01-01")
 
 
+# Sys.setlocale(category = "LC_ALL","Slovak")
 # ZACIATOK ----------------------------------------------------------------
-
 
 
 sidebar <- dashboardSidebar(
   # VYSKUSAT
   # includeCSS("styles.css"),
   width = 244,
-  sidebarMenu(
-    menuItem("Interaktívna mapa", tabName = "mapa", icon = icon("map")),
-    menuItem("Grafy", icon = icon("dashboard"), tabName = "grafy",
+  
+  sidebarMenu(id="tabs",
+# POLOZKY SIDEBAR  MENU ------------------------------------------------------------
+    menuItem("Interaktívna mapa", tabName = "mapa", icon = icon("map"), selected=TRUE),
+    menuItem("Grafy",  tabName = "grafy",icon = icon("line-chart"),
              badgeLabel = "new", badgeColor = "green"),
     menuItem("Source code", icon = icon("file-code-o"), 
-             href = "https://github.com/rstudio/shinydashboard/"),
-    sidebarSearchForm(textId = "searchText", buttonId = "searchButton",
-                      label = "Search..."),
+             href = "https://github.com/UrsaMinor-R/Tajchy"),
+# hr(),
+
+# OBSAH KARTY-INTERAKTIVNA MAPA -------------------------------------------
+conditionalPanel("input.tabs=='mapa'",
+                 fluidRow(
+                   column(1),
+                   column(10,
+                          # VYHLADAVAC
+                          sidebarSearchForm(textId = "searchText", buttonId = "searchButton",
+                                            label = "Search..."),
+                          # VYBER SKUPINY VHS
+                          selectInput(inputId ="skupina",
+                                      label = "Skupina tajchov:", c("Celý systém" = "celySystem",
+                                                                   "Piargske" = "piargske",
+                                                                   "Štiavnické"="stiavnicke",
+                                                                   "Hodrušské"="hodrusske",
+                                                                   "Vyhnianske"="vyhnianske",
+                                                                   "Belianske"="belianske",
+                                                                   "Kolpaššské"="kolpasske",
+                                                                   "Moderštôlnianske"="modersolnianske",
+                                                                   "Pukanské"="pukanske"),
+                                      selectize = TRUE,  selected = "celySystem", multiple = "output.multiple", 
+                          
+                          # ROZSAH - input$range
+                          sliderInput("range", "Rok:",
+                                      min_date, max_date, value = max_date, animate = TRUE, step = 50),
+                          
+                          checkboxGroupInput("checkbox", "Objects to show:",
+                                             c("Tajchy" = "tjch",
+                                               "Jarky" = "jrk",
+                                               "Štôlne" = "stl"),
+                                             selected = "tjch"),
+                          
+                          # selectInput - vyber obsahu tabulky-------------------------------------
+                          selectInput(inputId = "analyza",
+                                      label = "Typ analýzy",c("Existencia" = "existencia",
+                                                              "Kúpanie" = "kupanie",
+                                                              "Vznik" = "vznik",
+                                                              "Nadmorská výška" = "nadmV",
+                                                              "Plocha" = "plocha",
+                                                              "Dĺžka hrádze" = "dlzkHradze",
+                                                              "Výška hrádze" = "vyskHrazde",
+                                                              "Šírka hrádze" = "srkHradze",
+                                                              "Objem v 1000m3" = "obj1000m3",
+                                                              "Maximálna hĺbka v metroch" = "maxHlbkaM"),
+                                      selectize = TRUE, multiple = FALSE ),
+                          
+
+                          checkboxInput("legend", "Legend", TRUE)
+                   ))),
+
+    # tags$div(class="header", checked=NA,
+    #          tags$p("Ready to take the Shiny tutorial? If so"),
+    #          tags$a(href="shiny.rstudio.com/tutorial", "Click Here!")
+    # ),
     
-    h5("  Built with",
-       img(src = "logo_tajchy.png", height = "30px"),
-       "by",
-       img(src = "https://www.rstudio.com/wp-content/uploads/2014/07/RStudio-Logo-Blue-Gray.png", height = "30px"),
-       "."),
-    
-    selectInput(inputId ="skupina",
-                label = "Skupina tajchov", c("Piargske" = "piargske",
-                                             "Štiavnické"="stiavnicke",
-                                             "Hodrušské"="hodrusske",
-                                             "Vyhnianske"="vyhnianske",
-                                             "Belianske"="belianske",
-                                             "Kolpaššské"="kolpasske",
-                                             "Moderštôlnianske"="modersolnianske",
-                                             "Pukanské"="pukanske"),
-                selectize = TRUE, selected = "stiavnicke", multiple = TRUE),
-    
-    sliderInput("range", "Rok:",
-                min = 1500, max = 2000, value = 1600, animate = TRUE, step = 25),
-    
-    checkboxGroupInput("checkbox", "Objects to show:",
-                       c("Tajchy" = "tjch",
-                         "Jarky" = "jrk",
-                         "Štôlne" = "stl"),
-                       selected = "tjch"),
-    
-    tags$div(class="header", checked=NA,
-             tags$p("Ready to take the Shiny tutorial? If so"),
-             tags$a(href="shiny.rstudio.com/tutorial", "Click Here!")
-    ),
-    
-    # selectInput - vyber obsahu tabulky-------------------------------------
-    selectInput(inputId = "analyza",
-                label = "Typ analýzy",c("Existencia" = "existencia",
-                                        "Kúpanie" = "kupanie",
-                                        "Vznik" = "vznik",
-                                        "Nadmorská výška" = "nadmV",
-                                        "Plocha" = "plocha",
-                                        "Dĺžka hrádze" = "dlzkHradze",
-                                        "Výška hrádze" = "vyskHrazde",
-                                        "Šírka hrádze" = "srkHradze",
-                                        "Objem v 1000m3" = "obj1000m3",
-                                        "Maximálna hĺbka v metroch" = "maxHlbkaM"),
-                selectize = TRUE, multiple = FALSE )
-    
-    
+   
+
+h5("  Built with",
+   img(src = "https://ih0.redbubble.net/image.512525295.6965/flat,800x800,075,f.jpg", height = "30px"),
+   "by",
+   img(src = "https://www.rstudio.com/wp-content/uploads/2014/07/RStudio-Logo-Blue-Gray.png", height = "30px"),
+   ".")
   )
 )
 
@@ -89,8 +105,8 @@ body <-   dashboardBody(
             
             
             absolutePanel(id = "control", top = 80, left = "auto", right = 50, bottom = "auto",
-                          width="14%", height = "auto",
-                          fixed = TRUE, draggable = TRUE,
+                          width="11%", height = "auto",
+                          fixed = TRUE, draggable = FALSE,
                           
                           selectInput("bmap", "Base map tile provider", choices = c("OpenStreetMap.Mapnik",
                                                                                     "Esri.WorldStreetMap",
@@ -99,12 +115,20 @@ body <-   dashboardBody(
                                                                                     "Stamen.Watercolor",
                                                                                     "Stamen.Terrain",
                                                                                     "Stamen.Toner"), 
-                                      selected = "OpenStreetMap.Mapnik")
-            )
+                                      selected = "OpenStreetMap.Mapnik"),
+                                    actionButton("update", "Update Map!"))
+            ),
             
-            
-            
-    ),
+            # absolutePanel(
+            #   top = 60, left = "auto", right = 20, bottom = "auto",
+            #   width = 330, height = "auto",draggable = TRUE,
+            #   wellPanel(
+            #     selectInput("Suburb", "Select one Suburb:",choices = c("Select one Suburb" = "All")),
+            #     uiOutput("secondselection")
+            #   ),
+            #   style = "opacity: 0.65; z-index: 10;" ## z-index modification
+            # )
+        
     
     tabItem(tabName = "grafy",
             h3("2D a 3D Vizualizácia")
@@ -140,3 +164,5 @@ ui <- dashboardPage( #skin = "black",
   body
   
 )
+
+
